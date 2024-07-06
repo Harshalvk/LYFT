b69933b2aeebf66f0da07d80321ccb4930df6710
Harshalvk <harshalvkhobragade@gmail.com>
2024-07-06 22:55:13 +0530

    ADD: Types file

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type PartnersLogoType = {
+  id: number;
+  name: string;
+  logo: string;
+}[];
+
+export type MilestoneProps = {
+  title: string;
+  description: string;
+  lastItem: boolean;
+};
+
+export type RoadmapType = {
+  id: number;
+  name: string;
+  description: string;
+};
